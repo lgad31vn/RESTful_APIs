830f10322aee9340653437609a72316b16164462
lgad31vn <lgad31vn@gmail.com>
2022-01-01 19:24:42 -0600

    added async/await to controllers

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -5,22 +5,44 @@ import { ContactSchema } from '../models/models';
 // and leveraging the ContactSchema as the model for new contacts
 const Contact = mongoose.model('Contact', ContactSchema); //Contact database has the ContactSchema schema
 
-export const addNewContact = (req, res) => {
-  let newContact = new Contact(req.body);
+// get all contacts
+export const getContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find().sort({ date: 1 });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 
-  newContact.save((err, contact) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(contact);
-  });
+  // Another way
+  //   Contact.find({}, (err, contact) => {
+  //     if (err) {
+  //       res.send(err);
+  //     }
+  //     res.json(contact);
+  //   });
 };
 
-export const getContacts = (req, res) => {
-  Contact.find({}, (err, contact) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(contact);
-  });
+// add new contact
+export const addNewContact = async (req, res) => {
+  let newContact = new Contact(req.body);
+
+  const contact = await newContact.save();
+  res.json(contact);
+
+  //   let newContact = new Contact(req.body);
+
+  //   newContact.save((err, contact) => {
+  //     if (err) {
+  //       res.send(err);
+  //     }
+  //     res.send('New contact added');
+  //     res.json(contact);
+  //   });
+};
+
+// get contact with id
+export const getContactWithID = (req, res) => {
+  Contact.findById(req.params.contactID);
 };
